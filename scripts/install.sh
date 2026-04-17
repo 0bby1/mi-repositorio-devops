06061bf04c437394d4f3e4d44c253eb4b4dd15d2
Ubuntu <ubuntu@ip-172-31-39-185.ec2.internal>
2026-04-17 16:58:10 +0000

    Fix install.sh final

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu/app/app
 
 docker stop flask-app || true
 docker rm flask-app || true
+docker rmi flask-app || true
 
 docker build -t flask-app .
 docker run -d -p 5000:5000 flask-app
