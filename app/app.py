92698c7e809b2179c2efacc121b9f0ab7f3e1053
0bby1 <cesarava06@gmail.com>
2026-04-20 15:37:44 -0600

    Antes del commit final 2.0

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hola desde Flask en Docker y en Jenkins!, Antes del commit"})
+    return jsonify({"message": "Hola desde Flask en Docker y Jenkins!, Antes del commit"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
